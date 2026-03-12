bf0a5d101e491b1b0be2d0bf5fcdbe0905f583b1
OtavioAMSantos <otavio100111@gmail.com>
2026-03-12 19:24:19 -0300

    Criando Helper, Controller e Model da Sala

---
+++ application/helpers/geral_helper.php
@@ -0,0 +1,56 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+function verificarParam($atributos, $lista){
+    foreach($lista as $key => $value){
+        if(array_key_exists($key, get_object_vars($atributos))){
+            $estatus = 1;
+        }else{
+            $estatus = 0;
+            break;
+        }
+    }
+    if(count(get_object_vars($atributos)) != count($lista)){
+        $estatus = 0;
+    }
+    return $estatus;
+}
+
+function validarDados($valor, $tipo, $tamanhoZero = true){
+    if(is_null($valor) || $valor === ''){
+        return array('codigoHelper' => 2, 'msg' => 'Conteúdo nulo ou vazio.');
+    }
+    if($tamanhoZero && ($valor === 0 || $valor === '0')){
+        return array('codigoHelper' => 3, 'msg' => 'Conteúdo zerado.');
+    }
+    switch($tipo){
+        case 'int':
+            if(filter_var($valor, FILTER_VALIDATE_INT) === false){
+                return array('codigoHelper' => 4, 'msg' => 'Conteúdo não inteiro.');
+            }
+            break;
+        case 'string':
+            if(!is_string($valor) || trim($valor) === ''){
+                return array('codigoHelper' => 5, 'msg' => 'Conteúdo não é um texto.');
+            }
+            break;
+        case 'date':
+            if(!preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $valor, $match)){
+                return array('codigoHelper' => 6, 'msg' => 'Data em formato inválido.');
+            }else{
+                $d = DateTime::createFromFormat('Y-m-d', $valor);
+                if(($d->format('Y-m-d') === $valor) == false){
+                    return array('codigoHelper' => 6, 'msg' => 'Data inválida.');
+                }
+            }
+            break;
+        case 'hora':
+            if(!preg_match('/^(?:[01]\d|2[0-3]):[0-5]\d$/', $valor)){
+                return array('codigoHelper' => 7, 'msg' => 'Hora em formato inválido.');
+            }
+            break;
+        default:
+            return array('codigoHelper' => 0, 'msg' => 'Tipo de dado não definido.');
+    }
+    return array('codigoHelper' => 0, 'msg' => 'Validação correta.');
+}
